f8874e30e58cd0b1c054b2e23c2f436a9f8bb7fb
ramyagollapalli2000 <107127021+ramyagollapalli2000@users.noreply.github.com>
2022-06-13 01:32:35 +0530

    handson

---
+++ Mobile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConsoleDemo
+{
+    public class Mobile
+    {
+        private int IMENo;
+        public String Name;
+        public void MyMobile()
+        {
+            IMENo = 456;
+            Name = "Redmi";
+            Console.WriteLine("My Mobile is :"+Name);
+        }
+        public int addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int multiplication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
