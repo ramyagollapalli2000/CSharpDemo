00d050317280ade9be57a013b1cfda65c6d86c2f
ramyagollapalli2000 <107127021+ramyagollapalli2000@users.noreply.github.com>
2022-06-13 01:33:11 +0530

    handson

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConsoleDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MobileConnection obj=new MobileConnection();
+            obj.MyCalls();
+            obj.Dail();
+            int num1 = 10;
+            int num2 = 30;
+            int num3 = num2 - num1;
+            Console.WriteLine(num3);
+            Mobile mobileObj=new Mobile();//object creation
+            Console.WriteLine(mobileObj.addition(100,200));
+            Console.WriteLine(mobileObj.multiplication(10, 5));
+            mobileObj.MyMobile();//function calling using object 
+            Console.WriteLine("Welcome");
+           
+            Console.Read();
+
+        }
+    }
+}
